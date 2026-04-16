01545c3d90c95732916d8c66eec9e83ae5b0d82d
AndreNabais <andrefnabais@gmail.com>
2026-04-16 14:58:04 +0100

    Add serial commands for DSP control and metronome

--- Project_MIDI_sine.ino
+++ Project_MIDI_sine.ino
@@ -17,11 +17,15 @@ float currentFreq = 0.0;
 float currentGain = 0.0;
 float potVolume = -1.0;
 float distortionAmount = 0.0;
+elapsedMillis stepTimer;
+int currentStep = 0;
+float bpm = 120.0;
 
 unsigned long lastSerialMs = 0;
 const unsigned long SERIAL_INTERVAL_MS = 50; 
 
 void setup() {
+
   Serial.begin(115200);
   Serial.setTimeout(5); // Important for fast GUI response
   
@@ -35,6 +39,61 @@ void setup() {
 }
 
 void loop() {
+
+  if (Serial.available()) {
+    String incoming = Serial.readStringUntil('\n');
+    incoming.trim();
+
+    if (incoming.startsWith("D:")) {
+      float value = incoming.substring(2).toFloat();
+      myDsp.setDist(value);
+    }
+
+    else if (incoming.startsWith("W:")) {
+      int value = incoming.substring(2).toInt();
+      myDsp.setWaveform(value);
+    }
+
+    else if (incoming.startsWith("A:")) {
+      float value = incoming.substring(2).toFloat();
+      myDsp.setAttack(value);
+    }
+
+    else if (incoming.startsWith("R:")) {
+      float value = incoming.substring(2).toFloat();
+      myDsp.setRelease(value);
+    }
+
+    else if (incoming.startsWith("DRUM:")) {
+      String drum = incoming.substring(5);
+
+
+    if (drum == "kick") {
+        myDsp.kickActive = true;
+        myDsp.kickSampleCount = 0;
+    }
+    else if (drum == "snare") {
+        myDsp.snareActive = true;
+        myDsp.snareSampleCount = 0;
+    }
+    else if (drum == "hihat") {
+        myDsp.hihatActive = true;
+        myDsp.hihatSampleCount = 0;
+    }
+    else if (drum == "tom") {
+        myDsp.tomActive = true;
+        myDsp.tomSampleCount = 0;
+    }
+    else if (drum == "cowbell") {
+        myDsp.cowbellActive = true;
+        myDsp.cowbellSampleCount = 0;
+    }
+}
+}
+
+
+
+
   // 1. Power Button Logic (Toggle)
   static bool lastButtonState = HIGH;
   bool currentButtonState = digitalRead(POWER_BUTTON_PIN);
@@ -49,6 +108,24 @@ void loop() {
     delay(50); // Simple debounce
   }
   lastButtonState = currentButtonState;
+
+
+      // Calculate how long one 16th note lasts in ms
+    float msPerStep = 60000.0f / (bpm * 4.0f);
+
+    if (stepTimer >= msPerStep) {
+        stepTimer = 0;
+        
+        // Metronome Sound (Trigger Cowbell every 4 steps/1 beat)
+        if (currentStep % 4 == 0) {
+            myDsp.compute_cowbell();
+        }
+
+        // Check your interface pattern here
+        // if (pattern[currentStep] == KICK) myDsp.triggerKick();
+
+        currentStep = (currentStep + 1) % 16; // Loop every 16 steps
+    }
 
   // 2. Heartbeat LED
   static unsigned long lastBlink = 0;
