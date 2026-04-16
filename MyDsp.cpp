29586de92ef89acd591e54e54228e307549a571e
AndreNabais <andrefnabais@gmail.com>
2026-04-16 10:41:51 +0100

    Refactor MyDsp for improved envelope and sample tracking
    Updated the envelope handling and sample counting in MyDsp.

--- MyDsp.cpp
+++ MyDsp.cpp
@@ -22,14 +22,14 @@ void MyDsp::setGain(float g){
 }
 
 void MyDsp::noteOn(float velocity){
-  startTime = millis();
+  startSample = sampleCount;
   StartGain=velocity / 127.0;
   NoteActive = true;
   NoteReleased = false;
 }
 
 void MyDsp::noteOff(){
-  stopTime = millis();
+  stopSample = sampleCount;
   NoteActive = false;
   NoteReleased = true;
 }
@@ -39,26 +39,30 @@ void MyDsp::noteOff(){
 void MyDsp::Envelope(float attack_val,float release_val){
   if (NoteActive) {
     //ATTACK
-    float duration_begin = millis() - startTime;
-    setGain((StartGain)*min(duration_begin/attack_val,1));
+    float attack_samples = (attack_val/1000.0f)*SR;
+    float samples_elapsed = sampleCount - startSample;
+    setGain((StartGain)*min(samples_elapsed/attack_samples,1));
   }
   
+    //RELEASE
     else {
-    float duration_end = millis() - stopTime;
-    setGain((StartGain)*(1-duration_end/release_val));
+      float release_samples = (release_val/1000.0f)*SR;
+      float samples_elapsed = sampleCount - stopSample;
+      setGain((StartGain)*max((1-samples_elapsed/release_samples),0));
   }
 }
 
 void MyDsp::update(void) {
-  Envelope(attack_val, release_val);
   audio_block_t* outBlock[AUDIO_OUTPUTS];
   for (int channel = 0; channel < AUDIO_OUTPUTS; channel++) {
     outBlock[channel] = allocate();
     if (outBlock[channel]) {
       for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+        Envelope(attack_val, release_val);
         float currentSample = (sine.tick() * 2 - 1) * gain;
         currentSample = max(-1.0f, min(1.0f, currentSample));
         outBlock[channel]->data[i] = (int16_t)(currentSample * MULT_16);
+        sampleCount++;
       }
       transmit(outBlock[channel], channel);
       release(outBlock[channel]);
