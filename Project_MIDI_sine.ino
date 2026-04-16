d10df13cec14b6a552f1141bf4569873fa3dba7d
AndreNabais <andrefnabais@gmail.com>
2026-04-16 09:24:57 +0100

    Refactor MIDI note handling in Project_MIDI_sine

--- Project_MIDI_sine.ino
+++ Project_MIDI_sine.ino
@@ -7,6 +7,7 @@ AudioControlSGTL5000 audioShield;
 AudioConnection patchCord0(myDsp, 0, out, 0);
 AudioConnection patchCord1(myDsp, 0, out, 1);
 
+
 void setup() {
   AudioMemory(20);
   audioShield.enable();
@@ -22,11 +23,11 @@ void loop() {
     if (type == usbMIDI.NoteOn && velocity > 0) {
       float freq = 440.0 * pow(2.0, (note - 69) / 12.0);
       myDsp.setFreq(freq);
-      myDsp.setGain(velocity / 127.0);
-    }
+      myDsp.noteOn(velocity);
+      }
 
-    if (type == usbMIDI.NoteOff || (type == usbMIDI.NoteOn && velocity == 0)) {
-      myDsp.setGain(0.0);
+    else if (type == usbMIDI.NoteOff) {
+      myDsp.noteOff(); // Tell the DSP the note ended
     }
   }
 }
