0b1a048f6e238efbfe5fd8c149686b7045915d25
jonas-roun <153138224+jonas-roun@users.noreply.github.com>
2026-04-17 14:30:50 +0200

    Update Project_MIDI_sine.ino

--- Project_MIDI_sine.ino
+++ Project_MIDI_sine.ino
@@ -13,7 +13,10 @@ AudioConnection p3(voices[3], 0, mixer, 3);
 AudioConnection pMixL(mixer, 0, out, 0);
 AudioConnection pMixR(mixer, 0, out, 1);
 
-const int POWER_BUTTON_PIN = 2;
+const int POWER_BUTTON_PIN = 0;  // Power on Pin 0
+const int VOLUME_POT_PIN = A0;   // Volume on A0
+const int PITCH_BEND_PIN = A2;   // Bending on A2
+
 bool isPoweredOn = true;
 float potVolume = 0.5;
 float distortionAmount = 0.0;
@@ -25,37 +28,45 @@ void setup() {
   Serial.begin(115200);
   AudioMemory(120);
   audioShield.enable();
-  audioShield.volume(0.5);
-  for(int i=0; i<4; i++) mixer.gain(i, 0.25);
-  pinMode(POWER_BUTTON_PIN, INPUT_PULLUP);
+  audioShield.volume(1.0);
+  for(int i=0; i<4; i++) mixer.gain(i, 0.22f); 
+  
+  pinMode(POWER_BUTTON_PIN, INPUT_PULLUP); // Using internal pullup for Pin 0
 }
 
 void loop() {
-  static bool lastButtonState = HIGH;
+  // 1. Power Logic (Pin 0)
   bool currentButtonState = digitalRead(POWER_BUTTON_PIN);
-
+  static bool lastButtonState = HIGH;
   if (lastButtonState == HIGH && currentButtonState == LOW) {
     isPoweredOn = !isPoweredOn;
-    if (!isPoweredOn) for(int i=0; i<4; i++) voices[i].noteOff();
-    delay(50);
+    if (!isPoweredOn) {
+      for(int i=0; i<4; i++) voices[i].noteOff();
+    }
+    delay(150); // Debounce
   }
   lastButtonState = currentButtonState;
 
   if (isPoweredOn) {
-    float newVol = analogRead(A0) / 1023.0;
-    if (abs(newVol - potVolume) > 0.01) {
-      potVolume = newVol;
-      audioShield.volume(potVolume);
+    // 2. Pots
+    float v = analogRead(VOLUME_POT_PIN) / 1023.0;
+    if (abs(v - potVolume) > 0.01) { potVolume = v; audioShield.volume(v); }
+
+    int b = analogRead(PITCH_BEND_PIN);
+    float nb = powf(2.0f, ((b - 512.0f) / 512.0f * 2.0f) / 12.0f);
+    if (abs(nb - pitchBendMod) > 0.01) {
+        pitchBendMod = nb;
+        for(int i=0; i<4; i++) voices[i].setPitchBend(nb);
     }
 
+    // 3. MIDI
     if (usbMIDI.read()) {
       byte type = usbMIDI.getType();
       byte note = usbMIDI.getData1();
       byte vel  = usbMIDI.getData2();
-
       if (type == usbMIDI.NoteOn && vel > 0) {
         int vIdx = -1;
-        for (int i=0; i<4; i++) if (!voices[i].NoteActive && !voices[i].NoteReleased) { vIdx=i; break; }
+        for (int i=0; i<4; i++) if (!voices[i].NoteActive && !voices[i].NoteReleased) { vIdx = i; break; }
         if (vIdx == -1) vIdx = 0;
         voices[vIdx].setFreq(440.0 * pow(2.0, (note - 69) / 12.0));
         voices[vIdx].setMidiNote(note);
@@ -64,40 +75,42 @@ void loop() {
       else if (type == usbMIDI.NoteOff || (type == usbMIDI.NoteOn && vel == 0)) {
         for (int i=0; i<4; i++) if (voices[i].getMidiNote() == note) voices[i].noteOff();
       }
-      else if (type == usbMIDI.PitchBend) {
-        int bend = usbMIDI.getData1() + (usbMIDI.getData2() << 7);
-        pitchBendMod = powf(2.0f, ((bend - 8192.0f) / 8192.0f * 2.0f) / 12.0f);
-        for(int i=0; i<4; i++) voices[i].setPitchBend(pitchBendMod);
+    }
+  }
+
+  // 4. Commands & Metronome Triggers
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    if (input.startsWith("DRUM:")) {
+    String d = input.substring(5);
+    if (d == "m_h") for(int i=0; i<4; i++) voices[i].triggerMetroHigh();
+    if (d == "m_l") for(int i=0; i<4; i++) voices[i].triggerMetroLow();
+    if (d == "kick")  voices[0].triggerKick();
+    if (d == "snare") voices[0].triggerSnare();
+    if (d == "hihat") voices[0].triggerHihat();
+    if (d == "tom") voices[0].triggerTom();
+    if (d == "cowbell") voices[0].triggerCowbell();
+}else {
+      int cIdx = input.indexOf(':');
+      if (cIdx != -1) {
+        float val = input.substring(cIdx + 1).toFloat();
+        if (input.startsWith("D:")) { distortionAmount = val; for(int i=0; i<4; i++) voices[i].setDist(val); }
+        else if (input.startsWith("W:")) { for(int i=0; i<4; i++) voices[i].setWaveform((int)val); }
       }
     }
   }
 
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();
-    
-    if (input.startsWith("DRUM:")) {
-      String drum = input.substring(5);
-      if (drum == "kick") voices[0].triggerKick();
-      else if (drum == "snare") voices[0].triggerSnare();
-      else if (drum == "hihat") voices[0].triggerHihat();
-      else if (drum == "tom") voices[0].triggerTom();
-      else if (drum == "cowbell") voices[0].triggerCowbell();
-    } else {
-      float val = input.substring(2).toFloat();
-      if      (input.startsWith("D:"))  { distortionAmount = val; for(int i=0; i<4; i++) voices[i].setDist(val); }
-      else if (input.startsWith("W:"))  { for(int i=0; i<4; i++) voices[i].setWaveform((int)val); }
-      else if (input.startsWith("A:"))  { for(int i=0; i<4; i++) voices[i].setAttack(val); }
-      else if (input.startsWith("R:"))  { for(int i=0; i<4; i++) voices[i].setRelease(val); }
-      else if (input.startsWith("VB:")) { for(int i=0; i<4; i++) voices[i].setVibrato(val); }
-    }
-  }
-
-  unsigned long now = millis();
-  if (now - lastSerialMs >= SERIAL_INTERVAL_MS) {
-      lastSerialMs = now;
+  // 5. Sync
+  if (millis() - lastSerialMs >= SERIAL_INTERVAL_MS) {
+      lastSerialMs = millis();
       Serial.printf("V:%.3f,N:%d,N2:%d,N3:%d,N4:%d,G:0.800,D:%.3f,B:%.3f\n", 
-                    potVolume, voices[0].getMidiNote(), voices[1].getMidiNote(), 
-                    voices[2].getMidiNote(), voices[3].getMidiNote(), distortionAmount, pitchBendMod);
+              potVolume, 
+              voices[0].getMidiNote(), 
+              voices[1].getMidiNote(), 
+              voices[2].getMidiNote(), 
+              voices[3].getMidiNote(), 
+              distortionAmount, 
+              pitchBendMod);
   }
 }
